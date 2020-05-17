a7d1181282d7bcd957ddf3bc144124bc23d17420
unknown <nikiforiakoleksandr@gmail.com>
2020-05-17 22:43:41 +0300

    structure, gulp, styles

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('scss/main.scss')
+                .pipe(sass({errLogToConsole: true}))
+                .pipe(gulp.dest('css'))
+})
+
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', gulp.series('sass'))
+})
