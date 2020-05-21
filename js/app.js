ac93f264fce9927471d2e4e6ed983aa1b1a8bad4
unknown <nikiforiakoleksandr@gmail.com>
2020-05-21 21:43:21 +0300

    'add_record'

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ const btnOk = document.querySelector('.ok');
 const btnStart = document.querySelector('.start');
 const btnNxt= document.querySelector('.nxt');
 const timer = document.querySelector('.timer');
+const answer = document.querySelector('.answer');
+const record = document.querySelector('.record');
 let count;
 let interval;
 const words={
-    easy :['left', 'hello', 'cool', 'moon', 'ran','cow', 'good', 'frog', 'book', 'rain', 'rest', 'five', 'lime', 'hero', 'gold'],
-    medium:['rabbit', 'black', 'floor', 'tomato', 'apple', 'music', 'tower', 'window', 'pear'],
-    hard:['exelent', 'reverse', 'different', 'keyboard', 'conclusion', 'elephant', 'terminal']
+    easy :['left', 'hello', 'cool', 'moon', 'rain', 'green', 'good', 'frog', 'book', 'rain', 'rest', 'five', 'lime', 'hero', 'gold'],
+    medium:['rabbit', 'black', 'floor', 'tomato', 'plate', 'water', 'apple', 'music', 'tower', 'window', 'pear', 'alone'],
+    hard:['exelent', 'reverse', 'different', 'keyboard', 'conclusion', 'elephant', 'terminal', 'weather', 'yellow']
 };
 const {easy, medium, hard}=words;
 let val;
@@ -17,12 +19,15 @@ let level= document.querySelector('.level');
 let levelCount=1;
 let innerCount;
 let inptValue;
+let recordCount=0;
 let count2;
+let countR =0;
 const description = document.querySelector('.description');
 const btnColors = ['#DD4E42', '#FECE47', '#498AF4', '#169F5C', '#DD4E42', '#FECE47', '#498AF4', '#169F5C'];
 
 btnStart.addEventListener('click', function(e){
     timer.innerHTML='';
+    answer.innerHTML ='';
     btnOk.style.display='block';
     description.style.display='none';
     setTimeout(()=>{
@@ -37,13 +42,13 @@ btnStart.addEventListener('click', function(e){
     count=5;
     val= 'go';
     ex.innerHTML= val;
+    level.style.display='block'
     level.innerHTML=innerCount;
 
     interval= setInterval(()=>{
         count-=1
         count2 = count;
-        //hide problem
-        //
+        
         btnOk.innerHTML=count2;
         btnOk.style.backgroundColor=btnColors[count2];
         if(count<0){
@@ -61,9 +66,18 @@ btnOk.addEventListener('click', function(e){
     e.preventDefault();
     btnNxt.style.display='block';
     this.style.display='none';
+    level.style.display='none';
     if(inpt.value.split('').reverse().join('')=== val){
-        inptValue = inpt.value.split('').reverse().join('');
-        timer.innerHTML=`Bull's eye!  Answer : ${inptValue}`;
+        inptValue = inpt.value.split('').join('');
+        
+        answer.innerHTML = `${inptValue}`;
+
+        setTimeout(()=>{
+            timer.innerHTML=`Bull's eye!  Answer : `;
+            answer.innerHTML= `${inptValue.split('').reverse().join('')}`;
+          
+        },600)
+
         clearInterval(interval);
         count =0;
         ex.innerHTML='';
@@ -72,6 +86,7 @@ btnOk.addEventListener('click', function(e){
         
         innerCount = levelCount+=1;
         level.innerHTML=`Level : ${innerCount}`;
+        recordCount=innerCount-1;
         inpt.value='';
  
     }else{
@@ -80,15 +95,29 @@ btnOk.addEventListener('click', function(e){
         inptValue = inpt.value.split('').reverse().join('');
         timer.innerHTML=`Game Over! Answer : ${inptValue}`
         inpt.value='';
+        record.style.display='block';
+        
+        record.innerHTML=`Your record : ${countR}`
+        if(recordCount>countR){
+            countR=recordCount;
+            record.innerHTML=`Your record : Level ${countR}`
+
+        }
+    
+
+        
         clearInterval(interval);
     } 
 
 })
 btnNxt.addEventListener('click', function(e){
     e.preventDefault();
+    answer.innerHTML ='';
+    timer.innerHTML='';
     inpt.value='';
     this.style.display='none';
     btnOk.style.display='block';
+    level.style.display='block';
     setTimeout(()=>{
         ex.innerHTML='';
     
@@ -133,8 +162,6 @@ btnNxt.addEventListener('click', function(e){
             ex.innerHTML='';
         }
         
-        // timer.innerHTML=count2;
-        
         btnOk.style.backgroundColor=btnColors[count2];
         
         if(count<0){
@@ -143,6 +170,17 @@ btnNxt.addEventListener('click', function(e){
             btnStart.style.display='block';
             btnOk.style.display='none';
             timer.innerHTML=`Time is over :(`;
+            inpt.value='';
+
+            record.style.display='block'
+            record.innerHTML=`Your record : ${countR}`
+        if(recordCount>countR){
+            countR=recordCount;
+            record.innerHTML=`Your record : Level ${countR}`
+
+        }
+    
+          
             clearInterval(interval);
         }
     },1000)
